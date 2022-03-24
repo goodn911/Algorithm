536b21e4889d9b31755b7edee3bc33c178c32ecf
goodn911 <hanuli920@gmail.com>
2022-03-24 20:49:53 +0900

    특정문자뒤집기

---
+++ src/main/java/특정문자뒤집기.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class 특정문자뒤집기 {
+    public String solution(String str) {
+       String answer = "";
+       char[] x = str.toCharArray();
+       int lt =0 ,rt = str.length()-1;
+       while (lt<rt) {
+           if (!Character.isAlphabetic(x[lt])) {
+               lt++;
+           } else if (!Character.isAlphabetic(x[rt])) {
+               rt--;
+           } else {
+               char tmp = x[lt];
+               x[lt] = x[rt];
+               x[rt] = tmp;
+               lt++;
+               rt--;
+           }
+       }
+       answer = String.valueOf(x);
+
+
+       return answer;
+    }
+
+    public static void main(String[] args) {
+        특정문자뒤집기 solution = new 특정문자뒤집기();
+        Scanner kb = new Scanner(System.in);
+
+        String str=kb.next();
+        System.out.println(solution.solution(str));
+    }
+}
